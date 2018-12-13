070aa8b8d6fe9c464512b4188309f0f1bd51dd66
tory-mo <viktoria.moz@gmail.com>
2018-12-13 15:35:56 +0300

    simplified navigation structure

--- app/src/main/java/by/torymo/kotlinseries/ui/MainActivity.kt
+++ app/src/main/java/by/torymo/kotlinseries/ui/MainActivity.kt
@@ -2,12 +2,33 @@ package by.torymo.kotlinseries.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import by.torymo.kotlinseries.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val SELECTED_KEY = "selected_item"
+    private var selectedTab: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController = Navigation.findNavController(this, R.id.bottomNavFragment)
+
+//        if(savedInstanceState!=null && savedInstanceState.containsKey(SELECTED_KEY)){
+//            selectedTab = savedInstanceState.getInt(SELECTED_KEY)
+//            bottomNavigation.selectedItemId = selectedTab
+//        }
+
+        NavigationUI.setupWithNavController(findViewById<BottomNavigationView>(R.id.bottomNavigation),
+                navController)
     }
+
+//    override fun onSaveInstanceState(outState: Bundle) {
+//        outState.putInt(SELECTED_KEY, bottomNavigation?.selectedItemId)
+//    }
 }
